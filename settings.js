bae70598ae5632058237958f9917ce2b001bb0ed
RASH <112765499+MRRASH001@users.noreply.github.com>
2022-09-18 21:44:10 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,7 +20,7 @@ global.APIKeys = {
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = false //auto reading in gc (true to on, false to off)
-global.autobio = true  // auto bio system checking on Developing Leval
+global.autobio = false  // auto bio system checking on Developing Leval
 global.autoRecord = false //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
